825c101df6a30f35135cb478f36134cffec26afb
Aishwarya-codess <aishwaryasutone24@gmail.com>
2026-01-13 12:09:46 +0530

    day3: name and adding number

--- day3.py
+++ day3.py
@@ -1,2 +1,9 @@
-name = ("whats your name")
-print ("hello,", name)
+# Ask user their name
+name = input("whats your name?")
+print ("hello,",name, sep = " ")
+# Ask user a number
+a = int(input("Enter a number:"))
+# Ask another number
+b = int(input("Another number:"))
+# Add them
+print ("sum:", a+b)
